a48539560c067f5cd81f977a92027a724d45f9bd
Beren Hammill <bhammill9@gmail.com>
2023-12-17 12:59:58 +0800

    Inclusion of notes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Creates a variable containing the various elements required for random name generation. 
 const titleWords = {
     name: ['Gary', 'Elliot', 'Karen', 'Stacey', 'Bob', 'Mary'],
     articleOrPrep: ['the', 'of'],
@@ -5,10 +6,16 @@ const titleWords = {
     place: ['Tarth', 'Dragonsmere', 'Egguhfahm', 'Maffra', 'Sovereign Hill', 'Franga']
 }
 
+//Number generator used to randomly select indexes of various arrays.
 const generateRandomNumber = (num) => {
     return Math.floor(Math.random() * num);
 }
 
+/* 
+- Random index numbers are generated for each array within titleWords.
+- Each array is iterated through to find matching index. The value of which is pushed to personalTitle. 
+- If 'the' is selected for articleOrPrep, an adjective is then chosen to be pushed next. If not, a place is pushed next.  
+*/
 const titleGenerator = () => {
     
     let nameId = generateRandomNumber(titleWords.name.length);
@@ -45,6 +52,8 @@ const titleGenerator = () => {
     
 let personalTitle = [];
 
+
+// Joins the various array elements within personalTitle and embeds them within the sentence. Sentence is logged to the console. 
 let personalTitleGreeting = () => {
     const formatted = personalTitle.join(' ');
     console.log(`Greetings ${formatted} you outlandish devil`);
